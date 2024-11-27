ba809d461e12f91f6d66e1b5f7318a657fd3268c
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-28 00:00:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -25,3 +25,7 @@ Employee.prototype.jobGreet = function () {
     `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
   );
 };
+
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
